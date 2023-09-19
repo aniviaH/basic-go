091170cc833195b3b12a0b411e442cc80245eb1f
1058039938@qq.com <aniviah@163.com>
2023-09-19 19:50:14 +0800

    feat: 02-gin Web接口-Handler

---
+++ webook/internal/web/init_web.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"github.com/aniviaH/basic-go/webook/internal/web"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterRoutes() *gin.Engine {
+	server := gin.Default()
+
+	u := &web.UserHandler{}
+
+	// 注册
+	//server.POST("/users/signup", func(context *gin.Context) {
+	//
+	//})
+	// 这是 RESTful 风格
+	//server.PUT("/user", func(context *gin.Context) {
+	//
+	//})
+	server.POST("/users/signup", u.Gignup)
+
+	// 登录
+	//server.POST("/users/signin", func(context *gin.Context) {
+	//
+	//})
+	server.POST("/users/signin", u.Signin)
+
+	// 编辑用户
+	//server.POST("/users/edit", func(context *gin.Context) {
+	//
+	//})
+	// 这是 RESTful 风格
+	//server.POST("/users/:id", func(context *gin.Context) {
+	//
+	//})
+	server.POST("/users/edit", u.Edit)
+
+	// 用户信息
+	//server.GET("/users/profile", func(context *gin.Context) {
+	//
+	//})
+	// REST 风格
+	//server.GET("/users/:id", func(context *gin.Context) {
+	//
+	//})
+	server.GET("/users/profile", u.Profile)
+
+	//server.Run(":8080")
+
+	return server
+}
